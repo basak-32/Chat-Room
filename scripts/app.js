0455fe69a39b6b805d05d483a745bb36e10010be
basak-32 <ranajit1998basak@gmail.com>
2020-08-26 02:26:59 +0530

    fixed bug of appearing current message more than once

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
-const mssgForm = document.querySelector('.new-chat');
 const nameForm = document.querySelector('.new-name');
 const convoBody = document.querySelector('.btns-and-convo');
+const chatList = document.querySelector('.chat-list');
 const roomsButtons = document.querySelector('.chat-rooms');
+const mssgForm = document.querySelector('.new-chat');
+
 
 
 if (localStorage.getItem('username')) {
@@ -13,86 +15,229 @@ nameForm.addEventListener('submit', event => {
   event.preventDefault();
 
   const username = nameForm.name.value.trim();
-  console.log(username);
-  nameForm.reset();
-
-  chatroom.changeName(username);
 
   localStorage.setItem('username', username);
+
+  nameForm.name.value = '';
 
   nameForm.classList.add('d-none');
   convoBody.classList.remove('d-none');
 })
 
 
-updateUI();
+const renderChats = (doc) => {
+  if (doc.data().room === localStorage.getItem('room')) {
+    let li = document.createElement('li');
+    let username = document.createElement('span');
+    let message = document.createElement('span');
+    let time = document.createElement('div');
+
+    const currentTime = Date.now() / 1000;
+    const mssgTime = doc.data().created_at.seconds;
+    const secondsAgo = currentTime - mssgTime;
+
+    let ago = 0;
+    let unit = '';
+    if (secondsAgo / 60 < 1) {
+      ago = secondsAgo;
+      unit = 'seconds';
+    } else if (secondsAgo / (60 * 60) < 1) {
+      ago = secondsAgo / 60;
+      unit = 'minutes';
+    } else if (secondsAgo / (60 * 60 * 24) < 1) {
+      ago = secondsAgo / (60 * 60);
+      unit = 'hours';
+    } else if (secondsAgo / (60 * 60 * 24 * 7) < 1) {
+      ago = secondsAgo / (60 * 60 * 24);
+      unit = 'days';
+    } else if (secondsAgo / (60 * 60 * 24 * 7 * 4) < 1) {
+      ago = secondsAgo / (60 * 60 * 24 * 7);
+      unit = 'weeks';
+    } else if (secondsAgo / (60 * 60 * 24 * 7 * 4 * 12) < 1) {
+      ago = secondsAgo / (60 * 60 * 24 * 7 * 4);
+      unit = 'months';
+    } else if (secondsAgo / (60 * 60 * 24 * 7 * 4 * 12) >= 1) {
+      ago = secondsAgo / (60 * 60 * 24 * 7 * 4 * 12);
+      unit = 'years';
+    }
+
+    time.textContent = `${Math.round(ago)} ${unit} ago`;
+    time.classList.add('text-muted');
+
+
+    li.setAttribute('mssg-id', doc.id);
+
+    username.textContent = doc.data().username;
+    username.classList.add('username');
+
+    message.textContent = doc.data().message;
+
+    if (doc.data().username === localStorage.getItem('username')) {
+      li.appendChild(message);
+      li.classList.add('me');
+    } else {
+      li.appendChild(username);
+      li.appendChild(message);
+    }
+
+    li.appendChild(time);
+
+    chatList.appendChild(li);
+  }
+}
+
+
+if (!localStorage.getItem('room')) {
+  localStorage.setItem('room', 'general');
+}
+
+db.collection('chats').orderBy('created_at').onSnapshot(snapshot => {
+  // console.log(snapshot.docChanges());
+  let changes = snapshot.docChanges();
+
+  changes.forEach(change => {
+    // console.log(change);
+    if (change.type == 'added') {
+      renderChats(change.doc);
+    }
+  })
+})
+
+// db.collection('chats').get().then(snapshot => {
+//   snapshot.docs.forEach(doc => {
+//     renderChats(doc);
+//   });
+// })
+
+roomsButtons.addEventListener('click', event => {
+  // console.log(event.target.id);
+  localStorage.setItem('room', event.target.id);
+
+  chatList.innerHTML = '';
+  db.collection('chats').where('room', '==', localStorage.getItem('room')).orderBy('created_at').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+      renderChats(doc);
+    });
+  })
+})
 
 
 mssgForm.addEventListener('submit', event => {
   event.preventDefault();
 
-  message = mssgForm.message.value.trim();
+  const message = mssgForm.message.value.trim();
   // console.log(message);
-  chatroom.addChat(message)
-  .then(() => {
-    mssgForm.reset();
+  mssgForm.message.value = '';
+
+  const now = new Date();
+
+  db.collection('chats').add({
+    username: localStorage.getItem('username'),
+    room: localStorage.getItem('room'),
+    message: message,
+    created_at: firebase.firestore.Timestamp.fromDate(now)
   })
-  .catch(error => console.log(error));
 })
 
 
-roomsButtons.addEventListener('click', event => {
-  if (event.target.id === 'general') {
 
-    // console.log(event.target.id);
-    let room = event.target.id;
 
-    // console.log(list.innerHTML);
-    list.innerHTML = '';
 
-    localStorage.setItem('room', room);
+// const mssgForm = document.querySelector('.new-chat');
+// const nameForm = document.querySelector('.new-name');
+// const convoBody = document.querySelector('.btns-and-convo');
+// const roomsButtons = document.querySelector('.chat-rooms');
 
-    chatroom.changeRoom(room);
 
-    updateUI();
-  } else if (event.target.id === 'sports') {
+// if (localStorage.getItem('username')) {
+//   nameForm.classList.add('d-none');
+//   convoBody.classList.remove('d-none');
+// }
 
-    // console.log(event.target.id);
-    let room = event.target.id;
+// nameForm.addEventListener('submit', event => {
+//   event.preventDefault();
 
-    // console.log(list.innerHTML);
-    list.innerHTML = '';
+//   const username = nameForm.name.value.trim();
+//   console.log(username);
+//   nameForm.reset();
 
-    localStorage.setItem('room', room);
+//   chatroom.changeName(username);
 
-    chatroom.changeRoom(room);
+//   localStorage.setItem('username', username);
 
-    updateUI();
-  } else if (event.target.id === 'music') {
+//   nameForm.classList.add('d-none');
+//   convoBody.classList.remove('d-none');
+// })
 
-    // console.log(event.target.id);
-    let room = event.target.id;
 
-    // console.log(list.innerHTML);
-    list.innerHTML = '';
+// // updateUI();
 
-    localStorage.setItem('room', room);
 
-    chatroom.changeRoom(room);
+// mssgForm.addEventListener('submit', event => {
+//   event.preventDefault();
 
-    updateUI();
-  } else if (event.target.id === 'coding') {
+//   message = mssgForm.message.value.trim();
+//   // console.log(message);
+//   chatroom.addChat(message)
+//   .then(() => {
+//     mssgForm.reset();
+//   })
+//   .catch(error => console.log(error));
+// })
 
-    // console.log(event.target.id);
-    let room = event.target.id;
 
-    // console.log(list.innerHTML);
-    list.innerHTML = '';
+// roomsButtons.addEventListener('click', event => {
+//   if (event.target.id === 'general') {
 
-    localStorage.setItem('room', room);
+//     // console.log(event.target.id);
+//     let room = event.target.id;
 
-    chatroom.changeRoom(room);
+//     // console.log(list.innerHTML);
+//     list.innerHTML = '';
 
-    updateUI();
-  }
-});
+//     localStorage.setItem('room', room);
+
+//     chatroom.changeRoom(room);
+
+//     updateUI();
+//   } else if (event.target.id === 'sports') {
+
+//     // console.log(event.target.id);
+//     let room = event.target.id;
+
+//     // console.log(list.innerHTML);
+//     list.innerHTML = '';
+
+//     localStorage.setItem('room', room);
+
+//     chatroom.changeRoom(room);
+
+//     updateUI();
+//   } else if (event.target.id === 'music') {
+
+//     // console.log(event.target.id);
+//     let room = event.target.id;
+
+//     // console.log(list.innerHTML);
+//     list.innerHTML = '';
+
+//     localStorage.setItem('room', room);
+
+//     chatroom.changeRoom(room);
+
+//     updateUI();
+//   } else if (event.target.id === 'coding') {
+
+//     // console.log(event.target.id);
+//     let room = event.target.id;
+
+//     // console.log(list.innerHTML);
+//     list.innerHTML = '';
+
+//     localStorage.setItem('room', room);
+
+//     chatroom.changeRoom(room);
+
+//     updateUI();
+//   }
+// });
